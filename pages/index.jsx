2c1b43bb3bd07c549af0211c6867ea1cb6b441e5
Lorenzo Michelotti <lomichelotti@hotmail.com>
2022-08-23 01:02:00 -0300

    pagination

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,14 +5,16 @@ import { useEffect, useState, useRef } from "react";
 import Modal from "../common/Modal";
 import axios from "axios";
 import Swal from 'sweetalert2'
+import ReactPaginate from 'react-paginate';
 
-export default function Home({firestore}) {
+export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [wordList, setWordList] = useState([]);
+  const [wordList, setWordList] = useState({words: [], count: 0});
   const form = useRef();
+  const perPage = 12;
 
-  async function getWords() {
-    const resp = await axios(`${process.env.NEXT_PUBLIC_API}words`);
+  async function getWords(page = 1) {
+    const resp = await axios(`${process.env.NEXT_PUBLIC_API}words?page=${page}&perPage=${perPage}`);
     const data = await resp.data;
     setWordList(data);
     return data;
@@ -45,8 +47,11 @@ export default function Home({firestore}) {
     getWords();
   }
 
+  const handlePageClick = (event) => {
+    getWords(event.selected + 1);
+  };
+
   useEffect(() => {
-    console.log('effect');
     getWords();
   }, []);
 
@@ -58,9 +63,24 @@ export default function Home({firestore}) {
           A collection of words too good to go to waste in a regular dictionary.
         </div>
         
-        <div className="lg:w-[60%] mx-auto my-20 space-y-5">
-          {wordList.map((word, key) => <WordCard word={word} key={key}/>)}
+        <div className="lg:w-[60%] mx-auto mt-20 mb-10 space-y-5">
+          {wordList.words.map((word, key) => <WordCard word={word} key={key}/>)}
         </div>
+
+        <ReactPaginate
+        breakLabel="..."
+        nextLabel="next"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={Math.ceil(wordList.count/perPage)}
+        previousLabel="previous"
+        renderOnZeroPageCount={null}
+        className="flex space-x-1 mx-auto w-full justify-center mb-[10rem]"
+        pageLinkClassName="bg-slate-800 font-bold p-2 px-3 rounded-lg text-center"
+        previousLinkClassName="bg-slate-800 font-bold p-2 px-3 rounded-lg text-center"
+        nextLinkClassName="bg-slate-800 font-bold p-2 px-3 rounded-lg text-center"
+        activeLinkClassName="bg-slate-700 font-bold text-blue-500 p-2 px-3 rounded-lg text-center"
+        />
 
         <motion.button onClick={()=>setIsModalOpen(true)} whileTap={{scale: 0.8}} whileHover={{scale: 1.2}} className="fixed right-[2rem] bottom-[5rem]"><BsFillPlusCircleFill className="w-[3rem] h-[3rem]"></BsFillPlusCircleFill></motion.button>
 
@@ -69,25 +89,25 @@ export default function Home({firestore}) {
       <Modal title="Bring meaning to a word 🐣" isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
         <form ref={form} onSubmit={onSubmit} action="post">
           <div className="flex flex-col items-center space-y-3 my-10">
-            <div className="grid grid-cols-2 w-[50%]">
+            <div className="grid grid-cols-2 w-[90%] md:w-[70%]">
               <label  htmlFor="wordName">Word name:</label>
               <input maxLength="100" className="dark:bg-slate-50 text-slate-900 rounded-md p-1" type="text" name="wordName" id="wordName" />
             </div>
-            <div className="grid grid-cols-2 w-[50%]">
+            <div className="grid grid-cols-2 w-[90%] md:w-[70%]">
               <div>
                 <label htmlFor="def">Definition:</label>
                 <legend className="text-sm text-slate-500">max. length 100 chars</legend>
               </div>
               <textarea maxLength="100" className="dark:bg-slate-50 resize-none text-slate-900 rounded-md p-1 h-[5rem]" name="def" id="def" cols="30" rows="10"></textarea>
             </div>
-            <div className="grid grid-cols-2 w-[50%]">
+            <div className="grid grid-cols-2 w-[90%] md:w-[70%]">
               <div>
                 <label  htmlFor="def">Usage/Example:</label>
                 <legend className="text-sm text-slate-500">max. length 100 chars</legend>
               </div>
               <textarea maxLength="100" className="dark:bg-slate-50 resize-none text-slate-900 rounded-md p-1 h-[5rem]" name="usage" id="usage" cols="30" rows="10"></textarea>
             </div>
-            <div className="grid grid-cols-2 w-[50%]">
+            <div className="grid grid-cols-2 w-[90%] md:w-[70%]">
               <div>
                 <label  htmlFor="def">Creator name:</label>
               </div>
